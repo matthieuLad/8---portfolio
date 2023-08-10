7cb7899b4953201107d9f9303f13047d252f6122
TurboTruite <ladouce.matthieu@gmail.com>
2023-08-10 17:42:35 +0200

    CSS OK + htaccess

--- modules/modal.js
+++ modules/modal.js
@@ -1,14 +1,14 @@
 const openModal1 = function (e) {
-    e.preventDefault();
-    const target = document.getElementById("modal1");
-    target.style.display = "flex";
-    target.addEventListener("click", closeModal1);
-    target
-      .querySelector(".js-modal-stop")
-      .addEventListener("click", stopPropagation);
-    target.querySelector(".modal-close").addEventListener("click", closeModal1);
-  };
-  
+  e.preventDefault();
+  const target = document.getElementById("modal1");
+  target.style.display = "flex";
+  target.addEventListener("click", closeModal1);
+  target
+    .querySelector(".js-modal-stop")
+    .addEventListener("click", stopPropagation);
+  target.querySelector(".modal-close").addEventListener("click", closeModal1);
+};
+
 const closeModal1 = function (e) {
   e.preventDefault();
   const target = document.getElementById("modal1");
@@ -28,15 +28,15 @@ const openModal2 = function (e) {
 };
 
 const closeModal2 = function (e) {
-e.preventDefault();
-const target = document.getElementById("modal2");
-target.style.display = "none";
-target.removeEventListener("click", closeModal2);
+  e.preventDefault();
+  const target = document.getElementById("modal2");
+  target.style.display = "none";
+  target.removeEventListener("click", closeModal2);
 };
-  
+
 const stopPropagation = function (e) {
   e.stopPropagation();
 };
-  
-document.getElementById("js-modal1").addEventListener("click", openModal1); 
-document.getElementById("js-modal2").addEventListener("click", openModal2); 
+
+document.getElementById("js-modal1").addEventListener("click", openModal1);
+document.getElementById("js-modal2").addEventListener("click", openModal2);
